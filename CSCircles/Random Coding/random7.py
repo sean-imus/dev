1c8cfa9b1592314701969ce748d2315e7b732480
Sean Tietz <sean.tietz2@gmail.com>
2025-10-20 15:20:07 +0200

    Improve formatting and add comments in random7.py for clarity

--- CSCircles/Random Coding/random7.py
+++ CSCircles/Random Coding/random7.py
@@ -1,5 +1,10 @@
 standard_input = "Bella"
 
+# creates a name song based on input name
+
 name = input().lower()
 
-print(f"{name}, {name}, bo-b{name[1:]}\nbanana-fana fo-f{name[1:]}\nfee-fi-mo-m{name[1:]}\n{name}!")
+print(f"{name}, {name}, bo-b{name[1:]}\n"
+      f"banana-fana fo-f{name[1:]}\n"
+      f"fee-fi-mo-m{name[1:]}\n"
+      f"{name}!")
